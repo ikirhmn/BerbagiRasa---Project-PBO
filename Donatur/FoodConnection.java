3525ee25c03c2cd2a79261e2f2b10ef89d35bf3b
Orcaface17 <lordorca17@gmail.com>
2024-12-13 19:20:08 +0800

    Mengupdate Food Connection agar sesuai dengan skema database yang baru

--- Donatur/FoodConnection.java
+++ Donatur/FoodConnection.java
@@ -10,21 +10,23 @@ import java.util.List;
 import src.DatabaseConnection;
 
 public class FoodConnection {
-
-    public List<String[]> getFoods(int userId) {
+    public List<String[]> getFoods(int idDonatur) {
         List<String[]> foodList = new ArrayList<>();
-        String query = "SELECT name, portions, imagePath FROM foods WHERE userId = ?";
+        String query = "SELECT nama, porsi, photo_path FROM Makanan WHERE id_donatur = ?";
 
         try (Connection conn = DatabaseConnection.getConnection();
              PreparedStatement stmt = conn.prepareStatement(query)) {
 
-            stmt.setInt(1, userId);
+            // Set parameter untuk query
+            stmt.setInt(1, idDonatur);
+
+            // Eksekusi query dan baca hasilnya
             try (ResultSet rs = stmt.executeQuery()) {
                 while (rs.next()) {
-                    String name = rs.getString("name");
-                    String portions = String.valueOf(rs.getInt("portions"));
-                    String imagePath = rs.getString("imagePath");
-                    foodList.add(new String[]{name, portions, imagePath});
+                    String nama = rs.getString("nama");
+                    String porsi = String.valueOf(rs.getInt("porsi"));
+                    String photoPath = rs.getString("photo_path");
+                    foodList.add(new String[]{nama, porsi, photoPath});
                 }
             }
         } catch (SQLException e) {
@@ -33,4 +35,3 @@ public class FoodConnection {
         return foodList;
     }
 }
-
